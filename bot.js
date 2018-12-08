182fd87067af736a63020a7c4fbb7d05885dde44
Discord.js Arabic Developer <32039975+Bowlingtoolkit@users.noreply.github.com>
2018-12-08 08:45:58 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+client.on("ready", () => {
+    function rainbow() {
+      client.guilds.get('520246668508135424').roles.find("name", "Rainbow🌈").setColor("RANDOM");
+    }
+    setInterval(rainbow, 10000);//rainbow delay time (ms)
+  })
+  client.login(process.env.BOT_TOKEN)
